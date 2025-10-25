6ca0c2a21a726acae721db383f005a10815da24b
Rui Chen <rui@chenrui.dev>
2025-10-25 16:51:48 -0400

    gpgfrontend: update `depends_on`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/g/gpgfrontend.rb
+++ Casks/g/gpgfrontend.rb
@@ -52,7 +52,7 @@ cask "gpgfrontend" do
   homepage "https://gpgfrontend.bktus.com/"
 
   depends_on formula: "gnupg"
-  depends_on macos: ">= :monterey"
+  depends_on macos: ">= :ventura"
 
   app "GpgFrontend.app"
 
